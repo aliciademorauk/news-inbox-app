c77db921c2522489ad144effd4c1697704cb9adc
Alicia de Mora Losana <aliciademoralosana@yahoo.com>
2025-04-30 16:02:32 +0100

    Refactor without Rails-like controllers :(

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,7 @@
 import { Hono } from 'hono';
 import { logger } from 'hono/logger';
 // import { cors } from 'hono/cors';
-import { newsRoutes } from './routes/news';
+import { news } from './routes/news';
 
 export const app = new Hono();
 
@@ -9,7 +9,7 @@ app.use('*', logger());
 // app.use('*', cors());
 
 app.get('/', (c) => {
-  return c.text('Hello Newsletter')
+  return c.text('Wooo Health Check!')
 });
 
-app.route('/api/news', newsRoutes);
+app.route('/api/news', news);
--- backend/src/routes/news.ts
+++ backend/src/routes/news.ts
@@ -1,9 +1,58 @@
 import { Hono } from 'hono';
-import { newsController } from '../controllers/news';
+import {
+  getAllNews,
+  getTodaysNews,
+  getNewsById,
+  deleteNews
+} from '../services/news';
 
-export const newsRoutes = new Hono();
+export const news = new Hono();
 
-newsRoutes.get('/', newsController.getAllNews);
-newsRoutes.get('/today', newsController.getTodaysNews);
-newsRoutes.get('/:id', newsController.getNewsById);
-newsRoutes.delete('/:id', newsController.deleteNews);
+news.get('/', async (c) => {
+  try {
+    const allNews = await getAllNews();
+    return c.json({ ok: true, data: allNews });
+  } catch(e: any) {
+    return c.json({ ok: false, error: e.message }, 500);
+  }
+});
+
+news.get('/today', async (c) => {
+  try {
+    const todayNews = await getTodaysNews();
+    return c.json({ ok: true, data: todayNews });
+  } catch(e: any) {
+    return c.json({ ok: false, error: e.message }, 500);
+  }
+});
+
+news.get('/:id', async (c) => {
+  try {
+    const id = Number(c.req.param('id'));
+    if (isNaN(id)) return c.json({ ok: false, error: 'Invalid News ID' }, 404);
+
+    const newsPiece = await getNewsById(id);
+    if (!newsPiece) return c.json({ ok: false, error: 'Not Found' }, 404);
+
+    return c.json({ ok: true, data: newsPiece });
+  } catch(e: any) {
+    return c.json({ ok: false, error: e.message }, 500);
+  }
+});
+
+news.delete('/:id', async (c) => {
+  try {
+    const id = Number(c.req.param('id'));
+    if (isNaN(id)) return c.json({ ok: false, error: 'Invalid News ID' }, 404);
+
+    const toDelete = await deleteNews(id);
+
+    return c.json({ ok: true, data: toDelete });
+  } catch(e: any) {
+    if (e.code == 'P2025') {
+      return c.json({ ok: false, error: 'Not Found' }, 404);
+    } else {
+      return c.json({ ok: false, error: e.message }, 500);
+    }
+  }
+});
